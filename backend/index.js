dd91a6a8bd6a4c3759280b524a4adf73ca4841fb
IIITManjeet <manjeetpathak2003@gmail.com>
2023-12-29 19:54:46 +0530

    final-data

--- backend/index.js
+++ backend/index.js
@@ -9,51 +9,49 @@ app.use(cors());
 
 const upload = multer({ dest: "uploads/" });
 
-app.post(
-  "/upload",
-  upload.fields([
-    { name: "image", maxCount: 1 },
-    { name: "aadhar", maxCount: 1 },
-    { name: "tenth", maxCount: 1 },
-    { name: "birth", maxCount: 1 },
-  ]),
-  async (req, res) => {
+app.post("/upload", upload.any(), async (req, res) => {
+  try {
     // req.files contains the uploaded files
-    try {
-      const imageFile = req?.files["image"][0] || "";
-      const aadharFile = req?.files["aadhar"][0] || "";
-      const tenthFile = req?.files["tenth"][0] || "";
-      const birthFile = req?.files["birth"][0] || "";
-      // Upload files to Cloudinary
-      let imageResult, tenthResult, aadharResult, birthResult;
-      if (imageFile !== "") {
-        imageResult = await cloudinary.v2.uploader.upload(imageFile.path);
-      }
-      if (aadharFile !== "") {
-        aadharResult = await cloudinary.v2.uploader.upload(aadharFile.path);
-      }
-      if (imageFile !== "") {
-        tenthResult = await cloudinary.v2.uploader.upload(tenthFile.path);
-      }
-      if (imageFile !== "") {
-        birthResult = await cloudinary.v2.uploader.upload(birthFile.path);
-      }
-      const imageURL = imageResult?.secure_url || "";
-      const aadharURL = aadharResult?.secure_url || "";
-      const tenthURL = tenthResult?.secure_url || "";
-      const birthURL = birthResult?.secure_url || "";
-      res.json({
-        imageURL,
-        aadharURL,
-        tenthURL,
-        birthURL,
-      });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Failed to upload files" });
+    const imageFile =
+      req.files.find((file) => file.fieldname === "image") || null;
+    const aadharFile =
+      req.files.find((file) => file.fieldname === "aadhar") || null;
+    const tenthFile =
+      req.files.find((file) => file.fieldname === "tenth") || null;
+    const birthFile =
+      req.files.find((file) => file.fieldname === "birth") || null;
+
+    // Upload files to Cloudinary
+    let imageResult, tenthResult, aadharResult, birthResult;
+    if (imageFile) {
+      imageResult = await cloudinary.v2.uploader.upload(imageFile.path);
     }
+    if (aadharFile) {
+      aadharResult = await cloudinary.v2.uploader.upload(aadharFile.path);
+    }
+    if (tenthFile) {
+      tenthResult = await cloudinary.v2.uploader.upload(tenthFile.path);
+    }
+    if (birthFile) {
+      birthResult = await cloudinary.v2.uploader.upload(birthFile.path);
+    }
+
+    const imageURL = imageResult?.secure_url || "";
+    const aadharURL = aadharResult?.secure_url || "";
+    const tenthURL = tenthResult?.secure_url || "";
+    const birthURL = birthResult?.secure_url || "";
+
+    res.json({
+      imageURL,
+      aadharURL,
+      tenthURL,
+      birthURL,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to upload files" });
   }
-);
+});
 
 cloudinary.v2.config({
   cloud_name: "dud3qipec",
